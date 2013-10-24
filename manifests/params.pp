b417c13c3272f0323dd65a02959633477ec5d2e2
Kyle Anderson <kwa@yelp.com>
2013-10-24 16:44:49 -0700

    Made params class operate on $::osfamily instead of operatingsystem

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,7 @@ class kibana3::params {
   $log_dir              = undef
   $server_name          = "kibana3.${::fqdn}"
 
-  case $::operatingsystem {
+  case $::osfamily {
     'RedHat': {
       $config_dir         = '/usr/share/kibana3'
       $config_dir_mode    = '0755'
@@ -47,7 +47,7 @@ class kibana3::params {
     }
 
     default: {
-      fail "Operating system ${::operatingsystem} is not supported."
+      fail "OS Family ${::osfamily} is not supported."
     }
   }
 }
